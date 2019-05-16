c984472644acfc72340bb79244f2d7362ea0bf2b
Accidia95 <50694782+Accidia95@users.noreply.github.com>
2019-05-16 18:00:01 +0800

    Update FatTree.py

--- FatTree.py
+++ FatTree.py
@@ -4,7 +4,7 @@ from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController  
 from mininet.node import CPULimitedHost, Host, Node  
 from mininet.node import OVSKernelSwitch, UserSwitch  
-from mininet.node import IVSSwitch  
+# from mininet.node import IVSSwitch  
 from mininet.cli import CLI  
 from mininet.log import setLogLevel, info  
 from mininet.link import TCLink, Intf  
